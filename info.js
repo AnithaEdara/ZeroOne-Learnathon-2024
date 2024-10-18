a814d00cce2f3a819d539d756df39eb83a7bc0cf
AnithaEdara <2300031494@kluniversity.in>
2024-10-18 13:11:47 +0530

    Add files via upload
    2300031494_E.Anitha

---
+++ info.js
@@ -0,0 +1,5 @@
+// let a="anitha";
+// let a1="CSE";
+// let a2="2300031494";
+// console.log(a,a1,a2);
+console.log("anitha,cse,2300031494");
---
+++ map.js
@@ -0,0 +1,25 @@
+// let arr=[1,2,3,4,5];
+// let newar=arr.map(Math.sqrt);
+// console.log(newar);
+let a=10;
+let b=5;
+
+const add=(a,b)=>{
+    return a+b;
+}
+console.log(add(a,b));
+
+const sub=(a,b)=>{
+    return a-b;
+}
+console.log(sub(a,b));``
+
+const mul=(a,b)=>{
+    return a*b;
+}
+console.log(mul(a,b));
+
+const div=(a,b)=>{
+    return a/b;
+}
+console.log(div(a,b));
